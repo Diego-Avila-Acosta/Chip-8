48fa961bf45c1031423da762590ac9e0899270ab
Diego Avila <aviladiego2009@gmail.com>
2024-04-16 00:47:46 -0300

    Added decode function

--- src/chip.rs
+++ src/chip.rs
@@ -122,13 +122,11 @@ impl Chip8 {
         }
     }
 
-    pub fn run_instruction(&mut self, delta_time: f64, key_pressed: Option<u8>) -> bool {
+    pub fn run_instruction(&mut self, delta_time: f64, key_pressed: Option<u8>) {
         self.delay_timer.check(delta_time);
 
         let instruction = self.fetch();
-        
-        if instruction == 0x0000 { return true; }
-        false
+        let instruction_type = self.decode(instruction);
     }
 
     fn fetch(&mut self) -> u16{
@@ -136,5 +134,81 @@ impl Chip8 {
         self.pc += 2;
     
         instruction
+    }
+
+    fn decode(&self, instruction: u16) -> Instruction {
+        let i = (0xF000 & instruction) << 12;
+        let x = ((0x0F00 & instruction) << 8) as usize;
+        let y = ((0x00F0 & instruction) << 4) as usize;
+        let n = (0x000F & instruction) as u8;
+        let nn = (0x00FF & instruction) as u8;
+        let nnn = (0x0FFF & instruction) as u16;
+    
+        match i {
+            0x0 => {
+                match nn {
+                    0xE0 => Instruction::Display(Display::Clear),
+                    0xEE => Instruction::Subroutine(Subroutine::Return),
+                    _ => todo!()
+                }
+            },
+            0x1 => Instruction::Jump(nnn as usize),
+            0x2 => Instruction::Subroutine(Subroutine::Call(nnn as usize)),
+            0x3 => Instruction::Skip(Skip::SkipIfEqual, x, nn),
+            0x4 => Instruction::Skip(Skip::SkipIfNotEqual, x, nn),
+            0x5 => {
+                let nn = self.registers[y];
+                Instruction::Skip(Skip::SkipIfEqual, x, nn)
+            },
+            0x6 => Instruction::Register(Register::Set, x, nn),
+            0x7 => Instruction::Register(Register::Add, x, nn),
+            0x8 => {
+                let nn = self.registers[y];
+                match n {
+                    0x0 => Instruction::Register(Register::Set, x, nn),
+                    0x1 => Instruction::ArithmeticLogic(ArithmeticLogic::BitwiseOr, x, nn),
+                    0x2 => Instruction::ArithmeticLogic(ArithmeticLogic::BitwiseAnd, x, nn),
+                    0x3 => Instruction::ArithmeticLogic(ArithmeticLogic::BitwiseXor, x, nn),
+                    0x4 => Instruction::ArithmeticLogic(ArithmeticLogic::AddsWithCarry, x, nn),
+                    0x5 => Instruction::ArithmeticLogic(ArithmeticLogic::SubtractWithBorrow, x, nn),
+                    0x6 => Instruction::ArithmeticLogic(ArithmeticLogic::ShiftRight, x, nn),
+                    0x7 => Instruction::ArithmeticLogic(ArithmeticLogic::SubtractYWithBorrow, x, nn),
+                    0xE => Instruction::ArithmeticLogic(ArithmeticLogic::ShiftLeft, x, nn),
+                    _ => todo!()
+                }
+            },
+            0x9 => {
+                let nn = self.registers[y];
+                Instruction::Skip(Skip::SkipIfNotEqual, x, nn)
+            },
+            0xA => Instruction::IRegister(IRegister::Set(nnn)),
+            0xB => {
+                let nnn = nnn + self.registers[0] as u16;
+                Instruction::Jump(nnn as usize)
+            },
+            0xC => Instruction::RandomByte(x, nn),
+            0xE => {
+                match nn {
+                    0x9E => Instruction::Skip(Skip::SkipIfKeyPressed, x, 0),
+                    0xA1 => Instruction::Skip(Skip::SkipIfKeyNotPressed, x, 0),
+                    _ => todo!()
+                }
+            },
+            0xF => {
+                match nn {
+                    0x07 => Instruction::Register(Register::SetToDelayTimer, x, 0),
+                    0x0A => Instruction::WaitKeyPress(x),
+                    0x15 => Instruction::Timer(TimerInstruction::SetDelay, x),
+                    0x18 => Instruction::Timer(TimerInstruction::SetSound, x),
+                    0x1E => Instruction::IRegister(IRegister::AddRegister(x)),
+                    0x29 => Instruction::IRegister(IRegister::SetToLocationSprite(x)),
+                    0x33 => Instruction::StoreBCD(x),
+                    0x55 => Instruction::Register(Register::StoreInMemory, x, 0),
+                    0x65 => Instruction::Register(Register::ReadFromMemory, x, 0),
+                    _ => todo!()
+                }
+            },
+            _ => todo!()
+        }
     }
 }
